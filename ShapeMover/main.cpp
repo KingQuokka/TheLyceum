9e1576ee83876ba3899f67390ca65b2f5170d50d
King Quokka <98844619+KingQuokka@users.noreply.github.com>
2022-02-27 18:31:00 -0500

    WIP

--- ShapeMover/main.cpp
+++ ShapeMover/main.cpp
@@ -99,79 +99,79 @@ int main(int argc, char* argv[])
 {
        
     Shapes c("Circles");
-
     c.addShapesFromFile("ShapeConfig.txt");
-    c.print();
     
-    /*
-    // variables 
-    int cRadius = circle.CR;                   // set variable for cirlce radius 
+    for (Circle& c : c.getCircles())
+    {
 
-    // creating a window for the shpaes 
-    const int wWidth = 2000;                    // set window width
-    const int wHeight = 1800;                   // set window height 
-    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "PLEASE WORK!");
+        // variables 
+        int cRadius = c.getcRad();                   // set variable for cirlce radius 
+
+        // creating a window for the shpaes 
+        const int wWidth = 2000;                    // set window width
+        const int wHeight = 1800;                   // set window height 
+        sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "PLEASE WORK!");
     
-    // creating shapes that will appear in the window 
-    sf::CircleShape circle(cRadius);            // creates cirlce with given radius 
-    circle.setFillColor(sf::Color::Green);      // set circle color 
-    circle.setPosition(circles.X, circles.Y);   // set cirlce starting position
-    float circleMoveSpeedX = circles.SX;        // variable for x axis move on bounce 
-    float circleMoveSpeedY = circles.SY;        // variable for y axis move on bounce  
+        // creating shapes that will appear in the window 
+        sf::CircleShape circle(cRadius);            // creates cirlce with given radius 
+        circle.setFillColor(sf::Color::Green);      // set circle color 
+        circle.setPosition(c.getxPos(), c.getyPos());   // set cirlce starting position
+        float circleMoveSpeedX = c.getxSpeed();        // variable for x axis move on bounce 
+        float circleMoveSpeedY = c.getySpeed();        // variable for y axis move on bounce  
 
-    // loads font to display text
-    sf::Font myFont;
+        // loads font to display text
+        sf::Font myFont;
 
-    // load font from file
-    if(!myFont.loadFromFile("Roboto.ttf")) 
-    {
-        // error check
-        std::cerr << "COULD NOT LOAD FONT!\n\n";
-        system("pause");
-        exit(-1);
-    } 
-
-    // string, font, size
-    sf::Text text("SHAPE MOVER", myFont, 60);
-        
-    //  align text to bottom left corner
-    text.setPosition(0, wHeight - (float)text.getCharacterSize());
-
-    // main loop
-    while (window.isOpen())
-    {
-        // event handling
-        sf::Event event;
-        while (window.pollEvent(event))
+        // load font from file
+        if(!myFont.loadFromFile("Roboto.ttf")) 
         {
-            // window close event trigger
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }            
-        }
-
-        // bounce on boundry collisions
-        if(circle.getPosition() .y > wHeight - (cRadius * 2) || circle.getPosition() .y < 0)
-        {
-            circleMoveSpeedY *= -1.0;
-            circle.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            // error check
+            std::cerr << "COULD NOT LOAD FONT!\n\n";
+            system("pause");
+            exit(-1);
         } 
-        else if(circle.getPosition() .x > wWidth - (cRadius * 2) || circle.getPosition() .x < 0)
+
+        // string, font, size
+        sf::Text text("SHAPE MOVER", myFont, 60);
+        
+        // align text to bottom left corner
+        text.setPosition(0, wHeight - (float)text.getCharacterSize());
+
+        // main loop
+        while (window.isOpen())
         {
-            circleMoveSpeedX *= -1.0;
-            circle.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
-        }         
+            // event handling
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                // window close event trigger
+                if (event.type == sf::Event::Closed)
+                {
+                    window.close();
+                }            
+            }
 
-        // movement animation 
-        circle.setPosition(circle.getPosition() + sf::Vector2f(circleMoveSpeedX, circleMoveSpeedY));
+            // bounce on boundry collisions
+            if(circle.getPosition() .y > wHeight - (cRadius * 2) || circle.getPosition() .y < 0)
+            {
+                circleMoveSpeedY *= -1.0;
+                circle.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            } 
+            else if(circle.getPosition() .x > wWidth - (cRadius * 2) || circle.getPosition() .x < 0)
+            {
+                circleMoveSpeedX *= -1.0;
+                circle.setFillColor(sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            }         
 
-        // render functions
-        window.clear();             // clear window
-        window.draw(circle);        // draw cirlce in current positon
-        window.draw(text);          // draw text to window 
-        window.display();           // diplay to window 
-    }
+            // movement animation 
+            circle.setPosition(circle.getPosition() + sf::Vector2f(circleMoveSpeedX, circleMoveSpeedY));
 
+            // render functions
+            window.clear();             // clear window
+            window.draw(circle);        // draw cirlce in current positon
+            window.draw(text);          // draw text to window 
+            window.display();           // diplay to window 
+        }
+    };
     return 0;
 }
