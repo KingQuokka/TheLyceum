cfa4c772ff0cdd7b7058e137f492f9548329e621
King Quokka <98844619+KingQuokka@users.noreply.github.com>
2022-03-03 20:47:22 -0500

    WIP. Shapes share the same x/y speed somehow. Why?

--- ShapeMover/main.cpp
+++ ShapeMover/main.cpp
@@ -8,80 +8,68 @@
 
 int main(int argc, char* argv[])
 {
-    sf::CircleShape polygon;
-    std::vector<sf::CircleShape> poly;
-    std::ifstream fin("ShapeConfig.txt");
-        float x = 0;
-        float y = 0;
-        float xSpeed = 0;
-        float ySpeed = 0;
-        int   polyRadius = 0;
-        int   polyPoints = 0;
+    sf::CircleShape polygon;                // variable for shapes
+    std::vector<sf::CircleShape> poly;      // vector to hold polygons
+    std::ifstream fin("ShapeConfig.txt");   // polygon configurations
+        float x          = 0;               // point on x axis
+        float y          = 0;               // point on y axis
+        float xSpeed     = 0;               // speed on x axis
+        float ySpeed     = 0;               // speed on y axis
+        int   polyRadius = 0;               // polygon radius
+        int   polyPoints = 0;               // number of points on polygon
         
+    // reads in polygon configurations and pushes them to poly vector 
     while(fin >> x)
     {
         fin >> y >> xSpeed >> ySpeed >> polyRadius >> polyPoints;
-        polygon.setPosition(x,y);
-        polygon.setFillColor(sf::Color::Green);
-        polygon.setOrigin(xSpeed, ySpeed);
-        polygon.setRadius(polyRadius);
+        polygon.setPosition  (x,y);
+        polygon.setFillColor (sf::Color::Green);
+        polygon.setRadius    (polyRadius);
+        xSpeed;                             // how do I add this to the polygon pushback?
+        ySpeed;                             // x and y speed are not changing per polygon 
         polygon.setPointCount(polyPoints);
-        poly.push_back(polygon);
-    }              
+        poly.push_back       (polygon);
+    }   
+    // creating a window for the shpaes 
+    const int wWidth  = 2000;               // set window width
+    const int wHeight = 1800;               // set window height 
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "PLEASE WORK!");
+       
+    // loads font to display text
+    sf::Font myFont;
     
-        // creating a window for the shpaes 
-        const int wWidth = 2000;                     // set window width
-        const int wHeight = 1800;                    // set window height 
-        sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "PLEASE WORK!");
-        /*
-        // creating shapes that will appear in the window 
-        sf::CircleShape circle(polyRadius, polyPoints); // cirlce with given radius 
-        circle.setFillColor(sf::Color::Green);                 // circle color 
-        circle.setPosition(x, y);  // cirlce starting position
-        float circleMoveSpeedX = xSpeed;            // x axis move on bounce 
-        float circleMoveSpeedY = ySpeed;            // y axis move on bounce  
-        **/
-        // loads font to display text
-        sf::Font myFont;
+    // load font from file
+    if(!myFont.loadFromFile("Roboto.ttf")) 
+    {
+        // error check
+        std::cerr << "COULD NOT LOAD FONT!\n\n";
+        system("pause");
+        exit(-1);
+    } 
+    // string, font, size
+    sf::Text text("SHAPE MOVER", myFont, 60);
+    
+    // align text to bottom left corner
+    text.setPosition(0, wHeight - (float)text.getCharacterSize());
 
-        // load font from file
-        if(!myFont.loadFromFile("Roboto.ttf")) 
+    // main loop
+    while (window.isOpen())
+    {
+        // event handling
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            // error check
-            std::cerr << "COULD NOT LOAD FONT!\n\n";
-            system("pause");
-            exit(-1);
-        } 
-
-        // string, font, size
-        sf::Text text("SHAPE MOVER", myFont, 60);
-        
-        // align text to bottom left corner
-        text.setPosition(0, wHeight - (float)text.getCharacterSize());
-
-        // main loop
-        while (window.isOpen())
+            // window close event trigger
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }            
+        }
+        // render functions
+        window.clear();       // clear window
+        for(int i = 0; i < poly.size(); i++)
         {
-            // event handling
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                // window close event trigger
-                if (event.type == sf::Event::Closed)
-                {
-                    window.close();
-                }            
-            }
-
-           
-
-            
-
-            // render functions
-            window.clear();       // clear window
-            for(int i = 0; i < poly.size(); i++)
-            {
-             // y boundary collisions
+            // y boundary collisions
             if(poly[i].getPosition() .y > wHeight - (poly[i].getRadius() * 2) 
                 || poly[i].getPosition() .y < 0)
             {
@@ -106,7 +94,6 @@ int main(int argc, char* argv[])
             }
             window.draw(text);    // draw text to window 
             window.display();     // diplay to window 
-        }
-    
+        }    
     return 0;
 }
