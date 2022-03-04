1169f3c22e11ecc02ec0c907acd920c7fcde7c8e
King Quokka <98844619+KingQuokka@users.noreply.github.com>
2022-03-03 19:46:05 -0500

    WIP The shapes move as one now, almost there!

--- ShapeMover/main.cpp
+++ ShapeMover/main.cpp
@@ -17,84 +17,96 @@ int main(int argc, char* argv[])
         float ySpeed = 0;
         int   polyRadius = 0;
         int   polyPoints = 0;
+        
     while(fin >> x)
     {
         fin >> y >> xSpeed >> ySpeed >> polyRadius >> polyPoints;
         polygon.setPosition(x,y);
         polygon.setFillColor(sf::Color::Green);
-        float polygonMoveSpeedX = xSpeed;
-        float polygonMoveSpeedY = ySpeed;
+        polygon.setOrigin(xSpeed, ySpeed);
         polygon.setRadius(polyRadius);
         polygon.setPointCount(polyPoints);
         poly.push_back(polygon);
     }              
     
-    // creating a window for the shpaes 
-    const int wWidth = 2000;                     // set window width
-    const int wHeight = 1800;                    // set window height 
-    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "PLEASE WORK!");
-    
-    // loads font to display text
-    sf::Font myFont;
-    // load font from file
-    if(!myFont.loadFromFile("Roboto.ttf")) 
-    {
-        // error check
-        std::cerr << "COULD NOT LOAD FONT!\n\n";
-        system("pause");
-        exit(-1);
-    } 
+        // creating a window for the shpaes 
+        const int wWidth = 2000;                     // set window width
+        const int wHeight = 1800;                    // set window height 
+        sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "PLEASE WORK!");
+        /*
+        // creating shapes that will appear in the window 
+        sf::CircleShape circle(polyRadius, polyPoints); // cirlce with given radius 
+        circle.setFillColor(sf::Color::Green);                 // circle color 
+        circle.setPosition(x, y);  // cirlce starting position
+        float circleMoveSpeedX = xSpeed;            // x axis move on bounce 
+        float circleMoveSpeedY = ySpeed;            // y axis move on bounce  
+        **/
+        // loads font to display text
+        sf::Font myFont;
 
-    // string, font, size
-    sf::Text text("SHAPE MOVER", myFont, 60);
-        
-    // align text to bottom left corner
-    text.setPosition(0, wHeight - (float)text.getCharacterSize());
-
-    // main loop
-    while (window.isOpen())
-    {
-        // event handling
-        sf::Event event;
-        while (window.pollEvent(event))
+        // load font from file
+        if(!myFont.loadFromFile("Roboto.ttf")) 
         {
-            // window close event trigger
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }            
-        }
-        // y boundary collisions
-        if(polygon.getPosition() .y > wHeight - (polyRadius * 2) 
-            || polygon.getPosition() .y < 0)
-        {
-            // reveres direction on y axis and randomize color
-            ySpeed *= -1.0; 
-            polygon.setFillColor
-                (sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            // error check
+            std::cerr << "COULD NOT LOAD FONT!\n\n";
+            system("pause");
+            exit(-1);
         } 
-        // x boundary collision 
-        else if(polygon.getPosition() .x > wWidth - (polyRadius * 2) 
-            || polygon.getPosition() .x < 0)
-        {
-            // reverse direction on x axis and randomize color
-            xSpeed *= -1.0;
-            polygon.setFillColor
-                (sf::Color(rand() % 255, rand() % 255, rand() % 255));
-        }         
 
-        // movement animation 
-        polygon.setPosition(polygon.getPosition() 
+        // string, font, size
+        sf::Text text("SHAPE MOVER", myFont, 60);
+        
+        // align text to bottom left corner
+        text.setPosition(0, wHeight - (float)text.getCharacterSize());
+
+        // main loop
+        while (window.isOpen())
+        {
+            // event handling
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                // window close event trigger
+                if (event.type == sf::Event::Closed)
+                {
+                    window.close();
+                }            
+            }
+
+           
+
+            
+
+            // render functions
+            window.clear();       // clear window
+            for(int i = 0; i < poly.size(); i++)
+            {
+             // y boundary collisions
+            if(poly[i].getPosition() .y > wHeight - (poly[i].getRadius() * 2) 
+                || poly[i].getPosition() .y < 0)
+            {
+                // reveres direction on y axis and randomize color
+                ySpeed *= -1.0; 
+                poly[i].setFillColor
+                    (sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            } 
+            // x boundary collision 
+            else if(poly[i].getPosition() .x > wWidth - (poly[i].getRadius() * 2) 
+                || poly[i].getPosition() .x < 0)
+            {
+                // reverse direction on x axis and randomize color
+                xSpeed *= -1.0;
+                poly[i].setFillColor
+                    (sf::Color(rand() % 255, rand() % 255, rand() % 255));
+            }               
+            // movement animation 
+            poly[i].setPosition(poly[i].getPosition() 
             + sf::Vector2f(xSpeed, ySpeed));
-
-        // render functions
-        window.clear();       // clear window
-        for(int i = 0; i < poly.size(); i++)
-        {
             window.draw(poly[i]);  // draw cirlce in current positon
+            }
+            window.draw(text);    // draw text to window 
+            window.display();     // diplay to window 
         }
-        window.draw(text);    // draw text to window 
-        window.display();     // diplay to window 
-    }
+    
     return 0;
 }
