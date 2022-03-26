4ffd3151f5ae0e523c5c06df608133bee0995725
King Quokka <98844619+KingQuokka@users.noreply.github.com>
2022-03-26 16:11:15 -0400

    WIP

--- FromTheDeep/FromTheDeep.cpp
+++ FromTheDeep/FromTheDeep.cpp
@@ -151,6 +151,55 @@ int main()
 				cloud1Active = false;
 			}
 		}
+		// Cloud 2
+		if (!cloud2Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+			// How high is the cloud
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(-200, height);
+			cloud2Active = true;
+		}
+		else
+		{
+			spriteCloud2.setPosition(
+				spriteCloud2.getPosition().x 
+				+ (cloud2Speed * dt.asSeconds())
+				, spriteCloud2.getPosition().y);
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud2Active = false;
+			}
+		}
+		if (!cloud3Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+			// How high is the cloud
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(-200, height);
+			cloud3Active = true;
+		}
+		else
+		{
+			spriteCloud3.setPosition(
+				spriteCloud3.getPosition().x 
+				+ (cloud3Speed * dt.asSeconds())
+				, spriteCloud3.getPosition().y);
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud3Active = false;
+			}
+		}
 
 		/*
 		 *************************************
