f97a0cce95f5b10ba40b96f789ed96478dfdea8f
King Quokka <98844619+KingQuokka@users.noreply.github.com>
2022-05-01 19:09:36 -0400

    Nothing works anymore!

---
+++ SpaceInvader/SelectUIPanel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "UIPanel.h"
+
+class SelectUIPanel : public UIPanel
+{
+private:
+    void initialiseButtons();
+
+public:
+    SelectUIPanel(Vector2i res);
+    void virtual draw(RenderWindow& window);
+};
